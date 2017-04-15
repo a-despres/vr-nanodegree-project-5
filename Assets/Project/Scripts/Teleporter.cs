efefe7f85845f78a083cd68822b03868756ad0fb
a-despres <adespres@gmail.com>
2017-04-14 19:32:56 -0700

    Add Teleport Script
    This commit adds the teleport script. This script is called when the
    player places the cardboard reticle over a subject icon and presses the
    cardboard trigger. When the TeleportToMarker function is called the
    player’s position is changed to the position of the selected subject
    icon with a slight offset so that the player is standing next to the
    desk beneath the icon.

---
+++ Assets/Project/Scripts/Teleporter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+	private Camera camera;
+	private GameObject previousMarker;
+
+	// Use this for initialization
+	void Start () {
+		camera = GetComponentInChildren<Camera> ();
+	}
+
+	public void TeleportToMarker (GameObject marker) {
+
+		// Unhide previous marker
+		if (previousMarker != null) {
+			previousMarker.SetActive (true);
+		}
+
+		// Hide current marker
+		marker.SetActive (false);
+
+		// Move camera to marker with offset
+		camera.transform.position = new Vector3(marker.transform.position.x + 0.5f, marker.transform.position.y, marker.transform.position.z - 1);
+
+		// Set current marker as previous marker
+		previousMarker = marker;
+	}
+}
