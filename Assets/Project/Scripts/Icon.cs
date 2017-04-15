227b5af5646ee440ece2b0e00e3252a58549e7a7
a-despres <adespres@gmail.com>
2017-04-14 19:29:48 -0700

    Add Icon Rotation Animation and Icon Script
    This commit adds the icon rotation animation and icon script. This
    animation is added to the subject icons that will be placed in the
    scene. The animation will be triggered using the Icon script that is
    called when the player places the cardboard reticle over the icon. The
    associated script will also stop the animation when the reticle leaves
    the icon.

---
+++ Assets/Project/Scripts/Icon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Icon : MonoBehaviour {
+
+	private Animator animator;
+	private ParticleSystem particleSystem;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		particleSystem = GetComponentInChildren<ParticleSystem> ();
+	}
+	
+	public void StartSpin () {
+		animator.SetBool ("isSpinning", true);
+//		if (!particleSystem.isPlaying) {
+			particleSystem.Play ();
+//		}
+	}
+
+	public void StopSpin () {
+		animator.SetBool ("isSpinning", false);
+//		if (particleSystem.isPlaying) {
+			particleSystem.Stop ();
+//		}
+	}
+}
