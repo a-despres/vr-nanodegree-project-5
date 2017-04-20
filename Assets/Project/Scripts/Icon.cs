c9b09e7d184cf95a85fd39792065c1a269c554ef
a-despres <adespres@gmail.com>
2017-04-19 19:58:10 -0700

    Update Icon Class
    This commit updates the icon class to include four public variables
    that will contain the information to be passed into the InfoDisplay.
    This update also adds three new methods: ShowInfo, which displays the
    InfoDisplay; EnablePaper, which turns on the collider for the
    associated subject paper; and DisablePaper, which disables the collider
    for the associated subject paper.

--- Assets/Project/Scripts/Icon.cs
+++ Assets/Project/Scripts/Icon.cs
@@ -1,29 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Icon : MonoBehaviour {
 
+	public string title;
+	public string description;
+	public Texture photo;
+	public string caption;
+
+	public GameObject paper;
+
 	private Animator animator;
 	private ParticleSystem particleSystem;
+	private GameManager gameManager;
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator> ();
 		particleSystem = GetComponentInChildren<ParticleSystem> ();
+		gameManager = FindObjectOfType<GameManager> ();
 	}
 	
 	public void StartSpin () {
 		animator.SetBool ("isSpinning", true);
-//		if (!particleSystem.isPlaying) {
-			particleSystem.Play ();
-//		}
+		particleSystem.Play ();
 	}
 
 	public void StopSpin () {
 		animator.SetBool ("isSpinning", false);
-//		if (particleSystem.isPlaying) {
-			particleSystem.Stop ();
-//		}
+		particleSystem.Stop ();
+	}
+
+	public void ShowInfo () {
+		if (gameManager != null) {
+			DisablePaper ();
+			gameManager.ShowInfoDisplay (this);
+		}
+	}
+
+	public void EnablePaper () {
+		Collider collider = paper.GetComponent<Collider> ();
+		collider.enabled = true;
+	}
+
+	public void DisablePaper () {
+		Collider collider = paper.GetComponent<Collider> ();
+		collider.enabled = false;
 	}
 }
