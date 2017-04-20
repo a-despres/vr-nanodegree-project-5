24593261fd2583164743b2dcc06b5506f2f78662
a-despres <adespres@gmail.com>
2017-04-19 19:54:48 -0700

    Add InfoDisplay Prefab and Class
    The InfoDisplay is the information window that is displayed when a
    subject is selected. The InfoDisplay contains four elements: a photo, a
    photo caption, a title, and a description. This information will be
    passed in from the subject icon, which will be updated to include
    public variables for these elements.

---
+++ Assets/Project/Scripts/InfoDisplay.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GVR;
+
+public class InfoDisplay : MonoBehaviour {
+	public bool isActive = false;
+	public RawImage photo;
+	public Text caption;
+	public Text title;
+	public Text description;
+
+	public Texture fieldtrip;
+
+	private GameManager gameManager;
+
+	// Use this for initialization
+	void Start () {
+		gameManager = FindObjectOfType<GameManager> ();
+	}
+
+	public void StartExperience () {
+		gameManager.audioManager.PlayClick ();
+		GvrViewer camera = FindObjectOfType<GvrViewer> ();
+		camera.transform.position = new Vector3 (11, 2, 0);
+	}
+
+	public void Set (Icon subject) {
+		title.text = subject.title;
+		description.text = subject.description;
+		photo.texture = subject.photo;
+		caption.text = subject.caption;
+	}
+
+	public void Close () {
+		if (gameManager.infoDisplayInstance != null) {
+			gameManager.EnableIcons ();
+			gameManager.audioManager.PlayClick ();
+			Collider collider = gameManager.currentMarker.paper.GetComponent<Collider> ();
+			collider.enabled = true;
+			Destroy (gameObject);
+		} else {
+			print ("Error: Info Display could not be destroyed.");
+		}
+	}
+}
