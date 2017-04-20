218e7ebc19118da7561f282eb3b9c99bedacb6ac
a-despres <adespres@gmail.com>
2017-04-19 19:47:51 -0700

    Add AudioManager with Audio Clips
    This commit adds the AudioManager, a very simple component that
    contains an Audio Source and two pointers for audio clips. The audio
    clips that will be used in the scene are “click” and “chime.”

---
+++ Assets/Project/Scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	public AudioClip chime;
+	public AudioClip click;
+
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+	
+	public void PlayChime () {
+		if (audioSource != null) {
+			audioSource.clip = chime;
+			audioSource.Play ();
+		}
+	}
+
+	public void PlayClick () {
+		if (audioSource != null) {
+			audioSource.clip = click;
+			audioSource.Play ();
+		}
+	}
+}
