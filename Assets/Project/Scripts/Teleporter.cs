471b6aac54e140547264d1969aa64dc793337e69
a-despres <adespres@gmail.com>
2017-04-19 20:01:25 -0700

    Update Teleporter Class
    This commit adds references to the GameManager which is used to store a
    reference to the selected merker/icon. The camera variable has also
    been changed to type GvrViewer so that when the position of the camera
    changes, the parent object (GvrViewerMain) is moved instead of the
    camera itself. This was done to address some minor motion issues when
    moving the camera directly.

--- Assets/Project/Scripts/Teleporter.cs
+++ Assets/Project/Scripts/Teleporter.cs
@@ -1,26 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GVR;
 
 public class Teleporter : MonoBehaviour {
 
-	private Camera camera;
-	private GameObject previousMarker;
+	private GvrViewer camera;
+	private GameManager gameManager;
+	private Icon previousMarker;
 
 	// Use this for initialization
 	void Start () {
-		camera = GetComponentInChildren<Camera> ();
+		camera = GetComponentInChildren<GvrViewer> ();
+		gameManager = FindObjectOfType<GameManager> ();
 	}
 
-	public void TeleportToMarker (GameObject marker) {
+	public void TeleportToMarker (Icon marker) {
+
+		// Play Chime
+		gameManager.audioManager.PlayChime ();
+
+		// Set current marker in GameManager
+		gameManager.currentMarker = marker;
 
 		// Unhide previous marker
 		if (previousMarker != null) {
-			previousMarker.SetActive (true);
+			previousMarker.gameObject.SetActive (true);
 		}
 
+		// Activate Collider of Associated Paper
+		marker.EnablePaper ();
+
 		// Hide current marker
-		marker.SetActive (false);
+		marker.gameObject.SetActive (false);
 
 		// Move camera to marker with offset
 		camera.transform.position = new Vector3(marker.transform.position.x + 0.5f, marker.transform.position.y, marker.transform.position.z - 1);
