d78d99a3c1b925c3105565bd620c7f58b82c8b46
a-despres <adespres@gmail.com>
2017-04-19 20:03:55 -0700

    Add GameManager Class
    This commit adds the GameManager class which contains references to the
    infoDisplay prefab and instance, as well as references to all of the
    scenes icons and AudioManager. The GameManager is an essential part of
    the project as it serves as a center point for all scripts.

---
+++ Assets/Project/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public InfoDisplay infoDisplay;
+	public InfoDisplay infoDisplayInstance;
+	public Icon currentMarker;
+
+	private Camera camera;
+	public AudioManager audioManager;
+	private Icon[] icons;
+
+	// Use this for initialization
+	void Start () {
+		camera = FindObjectOfType<Camera> ();
+
+		// Find all of the icons in the scene
+		icons = FindObjectsOfType<Icon> ();
+	}
+
+	public void ShowInfoDisplay (Icon forSubject) {
+		infoDisplayInstance = Instantiate (infoDisplay);
+
+		if (infoDisplayInstance != null) {
+			audioManager.PlayClick ();
+			DisableIcons ();
+			DisablePapers ();
+			infoDisplayInstance.transform.position = new Vector3(camera.transform.position.x - 2, 2, camera.transform.position.z);
+			infoDisplayInstance.Set (forSubject);
+		} else {
+			print ("Error: Info Display Not Initialized");
+		}
+	}
+
+	public void DisablePapers () {
+		if (icons.Length > 0) {
+			foreach (Icon icon in icons) {
+				Collider collider = icon.paper.GetComponent<Collider> ();
+				collider.enabled = false;
+			}
+		}
+	}
+
+	public void DisableIcons () {
+		if (icons.Length > 0) {
+			foreach (Icon icon in icons) {
+				icon.gameObject.SetActive (false);
+			}
+		} else {
+			print ("Error: No Icons could be found.");
+		}
+	}
+
+	public void EnableIcons () {
+		if (icons.Length > 0) {
+			foreach (Icon icon in icons) {
+				if (icon != currentMarker) {
+					icon.gameObject.SetActive (true);
+				}
+			}
+		} else {
+			print ("Error: No Icons could be found.");
+		}
+	}
+}
