0ef28d80048ebb6640b0396db3398bdc719fa4cc
a-despres <adespres@gmail.com>
2017-04-21 10:39:32 -0700

    Add ToolTip Class and Prefab
    This commit adds the ToolTip class and Prefab. The ToolTip class reads
    the title of the subject icon. The actual displaying of the tooltip
    will be handled by the GameManager.

---
+++ Assets/Project/Scripts/ToolTip.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolTip : MonoBehaviour {
+
+	public Text title;
+
+	public void Set (Icon subject) {
+		title.text = subject.title;
+	}
+}
